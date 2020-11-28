77c548401f795416e0e79cc14b0ae28fb5b6636b
Marin Balabanov <marincomics@gmail.com>
2020-11-29 01:34:09 +0200

    Adaptations

--- front-page.php
+++ front-page.php
@@ -21,7 +21,50 @@ get_header();
 
         <div class="post-row">
             <div class="page-wide-col">
-                <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+                <div class="post-row">
+                        
+                    <?php
+                    if ( have_posts() ) : 
+                        if ( is_home() && ! is_front_page() ) :
+                            ?>
+                            <header>
+                                <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                            </header>
+                            <?php
+                        endif;
+
+                        /* Start the Loop */
+                        while ( have_posts() ) :
+                            ?>
+                        <div class="post-col-50">
+                        <?php 
+                            the_post();
+
+                            /*
+                            * Include the Post-Type-specific template for the content.
+                            * If you want to override this in a child theme, then include a file
+                            * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                            */
+                            get_template_part( 'template-parts/content', get_post_type() );
+                            ?>
+                            </div> <!-- End Post Col -->
+                    <?php
+                        endwhile;
+
+                        the_posts_navigation();
+
+                        else:
+                            get_template_part( 'template-parts/content', 'none' );
+                        ?>
+                        </div>
+                    <?php
+
+                    endif;
+                    ?>
+
+                </div>
+
             </div>
             <div class="page-side-col">
                 <?php get_sidebar(); ?>
